e0d0c397bf88c4a5be8305b5e44decc258d324d1
Francisca Medina <fbmedina@uc.cl>
2016-02-25 22:06:24 -0300

    300 en vez de 100 en el primer ejercicio. También se puso la variable fadeElem

--- scripts/ex01.js
+++ scripts/ex01.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
-    $(".fadeitem").css({
+    var fadeElem = $(".fadeitem");
+
+    fadeElem.css({
     	background: "url(images/vancouver1.jpg)",
     	backgroundSize: "cover",
     	backgroundPosition: "center center"
     })
-    .fadeTo(100, 0.3)
+    .fadeTo(300, 0.3)
     .on ({
     	mouseenter: (function() {
-    		$(this).fadeTo(100, 1);
+    		$(this).fadeTo(300, 1);
     	}),
     	mouseleave: (function() {
-    		$(this).fadeTo(100, 0.5);
+    		$(this).fadeTo(300, 0.5);
     	})
     });
 
