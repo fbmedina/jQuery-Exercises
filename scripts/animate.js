8a9a284aa687c22c3792688d5f516c81736c9620
Francisca Medina <fbmedina@uc.cl>
2016-02-25 23:46:54 -0300

    ejercicio 7 (enimate) listo

--- scripts/animate.js
+++ scripts/animate.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-    
+	$(document).keydown(function(e){
+	    if (e.which === 37) {
+			$("#rocket").finish().animate({
+				left: "-=50"
+			});
+	    } else if (e.which === 38) {
+			$("#rocket").finish().animate({
+	            top: "-=50"
+	        });
+	    } else if (e.which === 39) {
+	        $("#rocket").finish().animate({
+	            left: "+=50"
+	        });
+	    } else if (e.which === 40) {
+	        $("#rocket").finish().animate({
+	            top: "+=50"
+	        });
+	    }
+	}); 
 });
